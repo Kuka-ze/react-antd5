b78e7331b898fde4e9af334b7ba2bae6786c6682
帅泽 <vuejs1024@163.com>
2024-03-28 18:08:09 +0800

    安装eslit

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,10 @@ import React from 'react';
 import { Button } from 'antd';
 // import BrowserWidthListener from './components/BrowserWidthListener';
 
-
 const App: React.FC = () => (
-  <div className="App">
-    <Button type="primary">Button</Button>
-  </div>
+    <div className="App">
+        <Button type="primary">Button</Button>
+    </div>
 );
 
 export default App;
